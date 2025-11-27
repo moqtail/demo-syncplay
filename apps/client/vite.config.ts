a1d7c16dc929eb5436832f74c24f9e4c8738171c
Ali C. Begen <ali.begen@networked.media>
2025-11-27 08:47:17 -0800

    cleanup

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, '../../libs/moqtail-ts/src'),
-    },
-  },
-})
+});
